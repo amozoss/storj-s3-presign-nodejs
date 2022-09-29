8df0e0633ef223abea63342f68febb27f6c409dc
Dan Willoughby <amozoss@gmail.com>
2022-09-29 13:20:23 -0600

    Add S3Client

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+var { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
+
+const s3Client = new S3Client({
+  credentials: {
+    accessKeyId: process.env.ACCESS_KEY,
+    secretAccessKey: process.env.SECRET_KEY,
+  },
+  region: "us-1",
+  endpoint: "https://gateway.storjshare.io",
+});
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.post("/presign", async function (req, res, next) {
+  try {
+    let params = {
+      Bucket: "vtl-nodejs",
+      Key: req.body.key,
+    };
+
+    let command = new PutObjectCommand(params);
+    const signedUrl = await getSignedUrl(s3Client, command, {
+      expiresIn: 3600,
+    });
+    res.json({ url: signedUrl });
+  } catch (err) {
+    console.error(err);
+  }
 });
 
 module.exports = router;
