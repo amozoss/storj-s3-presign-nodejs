19c913f18f3084c93c880c07b4346eada6ad17ba
Dan Willoughby <amozoss@gmail.com>
2022-09-29 13:22:58 -0600

    Add CORS

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var cors = require("cors");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
 var app = express();
 
+app.use(cors({ origin: "http://localhost:3000" }));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
